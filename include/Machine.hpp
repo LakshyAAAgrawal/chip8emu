cf226faadd222de4dce632f836047ed0b90de888
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2020-10-12 02:20:12 +0530

    Add instructions for first_match

--- include/Machine.hpp
+++ include/Machine.hpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include "../include/GraphicEngine.hpp"
 #include <vector>
+#include <random>
 
 class ProgramLargerThanMemoryException: public std::exception {
   virtual const char* what() const throw()
@@ -14,7 +15,7 @@ class Machine{
 private:
 	std::vector<uint8_t> registers; // V0-VF
 	std::vector<uint8_t> memory; // Memory
-	uint16_t IR; // Index register
+	uint16_t I; // Index register
 	std::vector<uint8_t> stack; // Stack
 	uint8_t SP; // Stack Pointer
 	uint8_t DT; // Delay Timer
@@ -22,6 +23,7 @@ private:
 	GraphicEngine ge; // Holds Graphics and frame buffer
 	uint16_t PC; // Program Counter
 	void execute(uint16_t& opcode);
+	uint8_t random_byte();
 public:
 	Machine();
 	void setInst(std::vector<uint8_t>& prog, uint16_t start_addr);
--- src/Machine.cpp
+++ src/Machine.cpp
@@ -2,12 +2,20 @@
 #include <algorithm>
 #include <functional>
 #include <map>
+#include <iostream>
 
 Machine::Machine(){
 	registers = std::vector<uint8_t>(16);
 	stack = std::vector<uint8_t>(64);
 	memory = std::vector<uint8_t>(4096);
 	PC = 0x200;
+}
+
+uint8_t Machine::random_byte(){
+	std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0,255);
+	return dist(rng);
 }
 
 void Machine::setInst(std::vector<uint8_t>& prog, uint16_t start_addr){
@@ -51,18 +59,27 @@ void Machine::execute(uint16_t& opcode){
 		{0x8000, [this](uint16_t& op){}},
 	};
 	std::map<uint16_t, std::function<void(uint16_t&)>> first_match{
-		{0x0000, [this](uint16_t& op){}},
-		{0x1000, [this](uint16_t& op){}},
-		{0x2000, [this](uint16_t& op){}},
-		{0x3000, [this](uint16_t& op){}},
-		{0x4000, [this](uint16_t& op){}},
-		{0x6000, [this](uint16_t& op){}},
-		{0x7000, [this](uint16_t& op){}},
-		{0xa000, [this](uint16_t& op){}},
-		{0xb000, [this](uint16_t& op){}},
-		{0xc000, [this](uint16_t& op){}},
-		{0xd000, [this](uint16_t& op){}},
+		{0x0000, [this](uint16_t& op){}}, // To be ignored as per COWGOD
+		{0x1000, [this](uint16_t& op){ PC = (op & 0x0fff);}}, // JP addr
+		{0x2000, [this](uint16_t& op){ SP++; stack[SP] = PC; PC = (op & 0x0fff);}}, // CALL addr
+		{0x3000, [this](uint16_t& op){ PC += ((registers[(op & 0x0f00)>>8] == (op & 0x00ff))? 2: 0);}}, // 3xkk - SE Vx, byte
+		{0x4000, [this](uint16_t& op){ PC += ((registers[(op & 0x0f00)>>8] != (op & 0x00ff))? 2: 0);}}, // 4xkk - SNE Vx, byte
+		{0x6000, [this](uint16_t& op){ registers[(op & 0x0f00)>>8] = (op & 0x00ff);}}, // 6xkk - LD Vx, byte
+		{0x7000, [this](uint16_t& op){ registers[(op & 0x0f00)>>8] += (op & 0x00ff);}}, // 7xkk - ADD Vx, byte
+		{0xa000, [this](uint16_t& op){ I = (op & 0x0fff);}}, // Annn - LD I, addr
+		{0xb000, [this](uint16_t& op){ PC = registers[0] + (op & 0x0fff);}}, // Bnnn - JP V0, addr
+		{0xc000, [this](uint16_t& op){ registers[(op & 0x0f00)>>8] = (op & 0x00ff) & random_byte();}}, // Cxkk - RND Vx, byte
+		{0xd000, [this](uint16_t& op){}} // TODO - Dxyn - DRW Vx, Vy, nibble
 	};
+
+	auto it = direct_match.find(opcode);
+	if(it != direct_match.end()){}
+	else if((it = first_third_fourth_match.find(opcode & 0xf0ff)) != first_third_fourth_match.end()){}
+	else if((it = first_fourth_match.find(opcode & 0xf00f)) != first_fourth_match.end()){}
+	else if((it = first_match.find(opcode & 0xf000)) != first_match.end()){}
+
+	if(it != first_match.end()) (it->second)(opcode);
+	else std::cout << "No match found for " << std::hex << (int) opcode << "\n";
 }
 
 void Machine::runLoop(){
