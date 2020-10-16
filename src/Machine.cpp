803b7ba8d317adf7108125906ecfddd00a366a44
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2020-10-17 00:27:04 +0530

    Fix minor syntax error

--- src/Machine.cpp
+++ src/Machine.cpp
@@ -116,7 +116,7 @@ void Machine::execute(uint16_t& opcode){
 	if(it != first_match.end()) (it->second)(opcode);
 	else {
 		std::cout << "No match found for opcode " << std::hex << (int) opcode << "\n";
-		std::cout << "This could be because this ROM uses SCHIP or another extension which is not yet supported.\n"
+		std::cout << "This could be because this ROM uses SCHIP or another extension which is not yet supported.\n";
 		std::exit(0);
 	}
 }
